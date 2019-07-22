e2237c8acea6ad54ad7c1d297db5ec67fb5f4d79
Wojciech M. Zabołotny <wzab01@gmail.com>
2019-07-23 01:35:50 +0200

    Modified script for automatic generation of slaves.

--- fpga/project_art7sb/build.sh
+++ fpga/project_art7sb/build.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 set -e
+git submodule update --init --recursive
+(
+  cd ../extbus
+  ./gener_slaves.sh
+)
 vivado -mode batch -source eprj_create.tcl
 vivado -mode batch -source eprj_write.tcl
 vivado -mode batch -source eprj_build.tcl
