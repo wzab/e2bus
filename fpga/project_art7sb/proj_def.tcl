c152c018a4d4b267ec18638a2b97eae097b5245b
Wojciech M. Zabołotny <wzab01@gmail.com>
2019-07-29 13:45:15 +0200

    Ported to Vivado 2019.1.1

--- fpga/project_art7sb/proj_def.tcl
+++ fpga/project_art7sb/proj_def.tcl
@@ -7,9 +7,9 @@ set eprj_part "xc7a35tftg256-1"
 set eprj_target_language "VHDL"
 set eprj_simulator_language "Mixed"
 set eprj_default_lib "work"
-set eprj_synth_flow "Vivado Synthesis 2017"
+set eprj_synth_flow "Vivado Synthesis 2019"
 set eprj_synth_strategy "Flow_PerfOptimized_high"
-set eprj_impl_flow "Vivado Implementation 2017"
+set eprj_impl_flow "Vivado Implementation 2019"
 set eprj_impl_strategy "Performance_Retiming"
-set eprj_vivado_version "2017.4"
-set eprj_vivado_version_allow_upgrade false
+set eprj_vivado_version "2019.1.1"
+set eprj_vivado_version_allow_upgrade true
